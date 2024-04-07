24e3351708af37c2ca4eed377c7295f037e3d8c4
reuben <rickr6129@gmail.com>
2024-04-07 21:34:02 +0300

    Understood this lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Written my code here!
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const yourName = "Reuben Kamau"; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
